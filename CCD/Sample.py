7de87208b3c2661f193cc766286d655b6eafb061
Michael Rivera <mrivera.lee@gmail.com>
2013-02-23 00:04:56 -0500

    Added working leap through python

--- CCD/Sample.py
+++ CCD/Sample.py
@@ -5,7 +5,7 @@
 # https://developer.leapmotion.com/sdk_agreement, or another agreement         #
 # between Leap Motion and you, your company or other organization.             #
 ################################################################################
-
+# -*- coding: utf-8 -*-
 import Leap, sys
 
 
