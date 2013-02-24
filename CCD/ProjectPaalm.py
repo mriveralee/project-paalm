9b9d9f49d9ea7ed10bc30a3f3077bfc1fd4cac89
Michael Rivera <mrivera.lee@gmail.com>
2013-02-23 21:38:02 -0500

    Adds sorting fingers by x, and mapping effector tip positions to maya. -Need to do CCD

--- CCD/ProjectPaalm.py
+++ CCD/ProjectPaalm.py
@@ -6,14 +6,16 @@
 # between Leap Motion and you, your company or other organization.             #
 ################################################################################
 
-import Leap, sys
+import Leap, sys, copy
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
 
 
 #Listener for phaleangeal
-class PAListener(Leap.Listener) :
+class PAListener(Leap.Listener):
 
+
+   
     #On initilization of listener
     def on_it(self, controller):
         print "Initialized"
@@ -31,29 +33,134 @@ class PAListener(Leap.Listener) :
 
     #On Frame being read from the Leap Do something
     def on_frame(self, controller):
-        #This is where we will do CCD
-
-        # Get the most recent frame
+        # Get the most recent frame and report some basic information
         frame = controller.frame()
+        # print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
+        #      frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
+        #Check if we have hands
+        if not frame.hands.empty:
+            # Get the first hand (Let's assume only Right hand for now)
+            hand = frame.hands[0]
+
+            #Get Palm Position
+            palmPosition = hand.palm_position 
+
+            # Check if the hand has any fingers
+            fingers = hand.fingers
+
+            if not fingers.empty:
+                #Sort fingers to get order by 
+                fingers = sort_fingers_by_x(fingers)
+                #print fingers
+                #Get first finger (Let's assume the only finger is the INDEX finger)
+                indexFinger = fingers[0]
+                #Fingers have direction, length, width, tip_velocity, tip_position, etc.
+                #Not sure what I need so will have to figure that out later
+                #indexPosition = indexFinger.tip_position
+                #indexWidth = indexFinger.width
+                #indexTipVelocity = 
+                
+                indexDir = indexFinger.direction
+                indexLength = indexFinger.length
+                
+                ''' 
+                 # The plan is to use the Direction of the finger vector to 
+                 # create a point in Maya's 3D space from the rigged-finger 
+                 # base-joint (the knuckle). With this point. We then run ccd
+                 # on the rigged hand. We use the length ratio of the 
+                 # Leap finger to the Maya Rigged Finger to move the finger appropriately
+                '''
+                #Length of effector in Maya
+                mayaEffectorLength = 10
+                #Map the finger direction to maya
+                mayaDir = map_dir_to_maya(indexDir, mayaEffectorLength)
+                #Effector Base (knuckle) Position in Maya
+                #mayaEffectorBasePos = # {x,y,z}
+                #The new tip location in maya
+                mTipPosition = get_maya_effector_tip_pos(mayaDir, mayaEffectorBasePos)
+                #print "Index Location: " + str(indexFinger.tip_position) + " Palm Position: " + str(palmPosition)
+                #NOW RUN CCD
+
 
 
 
     #CCD algorithm
     def perform_ccd(frame):
-
-
-
-
-
+        print "ccd test"
 ######################
 
+#Gets the Effector Tip Position using a maya mapped Direction and the base(knuckle position)
+def get_maya_effector_tip_pos(effectorDir, effectorBasePos) :
+    return effectorDir + effectorBasePos;
+
+
+
+#Gets a direction vector in maya space
+def map_dir_to_maya(leapFingerDir, mayaEffectorLength) :
+    #takes a normal vector in 3D space from the leap,
+    #maps it to a vector in Maya's 3D space that is used to 
+    #compute a new end effector position in maya
+    return mayaEffectorLength*leapFingerDir;
+
+
+
+
+
+# Selection Sort the fingers based on  x-position 
+# (left-most == Thumb, Right Hand OR Pinky, Left Hand)
+def sort_fingers_by_x(fingers):  
+    sortedFingers = [];
+    #Make a list of the fingers that isn't const (FingerList is)
+    for finger in fingers:
+        sortedFingers.append(finger)
+        print finger
+    #selection sort the fingers
+    for i in range(0, len(sortedFingers)):
+        min = i
+        for j in range(i+1, len(sortedFingers)):
+            x1 = sortedFingers[i].tip_position.x
+            x2 = sortedFingers[j].tip_position.x
+            if (x2 < x1):
+                min = j
+        #Swap the min (leftmost)
+        temp = sortedFingers[i]
+        sortedFingers[i] = sortedFingers[min]
+        sortedFingers[min] = temp
+    return sortedFingers
+
+
+
+
+def main():
+    # Create a sample listener and controller
+    
+    #Sample Listener usage
+    #listener = SampleListener()
+    listener = PAListener()        
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    sys.stdin.readline()
+
+    # Remove the sample listener when done
+    controller.remove_listener(listener)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
 
 
 
-
+##################################################################
+##################### SAMPLE EXAMPLE LISTENER ####################
+##################################################################
 
 class SampleListener(Leap.Listener):
     def on_init(self, controller):
@@ -164,22 +271,3 @@ class SampleListener(Leap.Listener):
 
         if state == Leap.Gesture.STATE_INVALID:
             return "STATE_INVALID"
-
-def main():
-    # Create a sample listener and controller
-    listener = SampleListener()
-    controller = Leap.Controller()
-
-    # Have the sample listener receive events from the controller
-    controller.add_listener(listener)
-
-    # Keep this process running until Enter is pressed
-    print "Press Enter to quit..."
-    sys.stdin.readline()
-
-    # Remove the sample listener when done
-    controller.remove_listener(listener)
-
-
-if __name__ == "__main__":
-    main()
